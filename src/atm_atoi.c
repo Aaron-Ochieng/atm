1082757dcfe02171ea339c2352652f8998e79c73
Aaron Ochieng <ochiengaoron@gmail.com>
2024-07-06 12:48:03 +0300

    Fix atoi function Removed for loop. It now uses while loop to check for end of string char value.

--- src/atm_atoi.c
+++ src/atm_atoi.c
@@ -1,14 +1,14 @@
-#include "string.h"
 
-int atm_atoi(char *input) {
-  int val;
-  int len = strlen(input);
-  for (int i = 0; i < len; i++) {
-    if (input[i] >= '1' && input[i] <= '9') {
-      val = val * 10 + input[i] - '0';
+int atm_atoi(const char *input) {
+  int val = 0;
+  int i = 0;
+  while (input[i] != '\0') {
+    if (input[i] >= '0' && input[i] <= '9') {
+      val = val * 10 + (input[i] - '0');
     } else {
       return -1;
     }
+    i++;
   }
   return val;
 }
