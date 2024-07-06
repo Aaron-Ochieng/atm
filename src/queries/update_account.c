bb3b2a5aeec22c518f710976b5617172d463e16e
Aaron Ochieng <ochiengaoron@gmail.com>
2024-07-06 10:51:33 +0300

    Add update phone record in the db The update_phone function takes account-number,phone,and userid to effectively update these values.

--- src/queries/update_account.c
+++ src/queries/update_account.c
@@ -35,3 +35,38 @@ int update_country(int acc_number, char *country, int user_id) {
   close_db_con();
   return SQLITE_OK;
 }
+
+int update_phone(int acc_number, int phone, int user_id) {
+  sqlite3_stmt *stmt;
+
+  int rc = initialize_db_conn();
+
+  if (rc != SQLITE_OK) {
+    close_db_con();
+    return SQLITE_ERROR;
+  }
+
+  char *sql =
+      "UPDATE Records SET phone = ? WHERE accountNbr = ? AND user_id = ?";
+  rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+
+  if (rc != SQLITE_OK) {
+    log_error(sqlite3_errmsg(db));
+    close_db_con();
+    return SQLITE_ERROR;
+  }
+
+  sqlite3_bind_int(stmt, 1, phone);
+  sqlite3_bind_int(stmt, 2, acc_number);
+  sqlite3_bind_int(stmt, 3, user_id);
+
+  if ((rc = sqlite3_step(stmt)) != SQLITE_DONE) {
+    log_error(sqlite3_errmsg(db));
+    close_db_con();
+    return SQLITE_ERROR;
+  }
+
+  sqlite3_finalize(stmt);
+  close_db_con();
+  return SQLITE_OK;
+}
