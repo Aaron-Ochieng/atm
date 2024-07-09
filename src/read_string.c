99e151339a1c48a3013000600ac88f1fa66b610c
Aaron Ochieng <ochiengaoron@gmail.com>
2024-07-09 12:17:04 +0300

    Deleted unused header file include

--- src/read_string.c
+++ src/read_string.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 
 // Function to read a string from standard input
-void read_string(char *buffer, size_t size)
-{
+void read_string(char *buffer, size_t size) {
   int c;
   // Clear any remaining newline characters from input buffer
-  while ((c = getchar()) != '\n' && c != EOF);
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
   fgets(buffer, size, stdin); // Read the entire line, including spaces
 }
