c57ed05422e55fbc44d90fce18e521daec0de1e0
Aaron Ochieng <ochiengaoron@gmail.com>
2024-07-09 12:16:13 +0300

    Added project header files

--- src/header.h
+++ src/header.h
@@ -2,31 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Date
-{
-    int month, day, year;
+struct Date {
+  int month, day, year;
 };
 
 // all fields for each record of an account
-struct Record
-{
-    int id;
-    int userId;
-    char name[100];
-    char country[100];
-    int phone;
-    char accountType[10];
-    int accountNbr;
-    double amount;
-    struct Date deposit;
-    struct Date withdraw;
+struct Record {
+  int id;
+  int userId;
+  char name[100];
+  char country[100];
+  int phone;
+  char accountType[10];
+  int accountNbr;
+  double amount;
+  struct Date deposit;
+  struct Date withdraw;
 };
 
-struct User
-{
-    int id;
-    char name[50];
-    char password[50];
+struct User {
+  int id;
+  char name[50];
+  char password[50];
 };
 
 // authentication functions
@@ -38,3 +35,9 @@ const char *getPassword(struct User u);
 void createNewAcc(struct User u);
 void mainMenu(struct User u);
 void checkAllAccounts(struct User u);
+
+// Other functions
+int atm_atoi(const char *input);
+void updateAccountInfo(struct User u);
+int new_record(struct Record r, char *name);
+void read_string(char *buffer, size_t size);
