5cc3ac384b8847dfad7e43ea2ebddc5d0e73b8f1
Aaron Ochieng <ochiengaoron@gmail.com>
2024-07-10 14:18:57 +0300

    Added function for withdrawal This ensures that a user withdrawal amount does not superciede the one that he/she currently has.

--- src/queries/transact_db.c
+++ src/queries/transact_db.c
@@ -36,6 +36,56 @@ double check_amount(int account_number, int user_id) {
     return -1;
   }
   sqlite3_finalize(stmt);
-  sqlite3_close(db);
+  close_db_con();
   return amount;
+}
+
+int withdraw_from_account(int account_number, double amount_to_deduct,
+                          int user_id) {
+  sqlite3_stmt *stmt;
+  double accountBalance;
+
+  double amountInAccount = check_amount(account_number, user_id);
+  if (amount_to_deduct > amountInAccount) {
+    return -10;
+  }
+  //
+  int rc = initialize_db_conn();
+  if (rc != SQLITE_OK) {
+    log_error(sqlite3_errmsg(db));
+    sqlite3_close(db);
+  }
+  //
+  accountBalance = amountInAccount - amount_to_deduct;
+  char *sql =
+      "UPDATE Records SET amount = ? WHERE accountNbr = ? AND userId = ?";
+
+  rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+  if (rc != SQLITE_OK) {
+    log_error(sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return SQLITE_ERROR;
+  }
+
+  sqlite3_bind_int(stmt, 1, accountBalance);
+  sqlite3_bind_int(stmt, 2, account_number);
+  sqlite3_bind_int(stmt, 3, user_id);
+
+  //
+  rc = sqlite3_step(stmt);
+  if (rc != SQLITE_DONE) {
+    log_error(sqlite3_errmsg(db));
+    close_db_con();
+    return SQLITE_ERROR;
+  }
+  if (rc != SQLITE_OK) {
+    log_error(sqlite3_errmsg(db));
+    close_db_con();
+    return SQLITE_ERROR;
+  }
+
+  sqlite3_finalize(stmt);
+  sqlite3_close(db);
+
+  return SQLITE_OK;
 }
