1f059607db6a41e01a16b76d9f325def72573d28
Aaron Ochieng <ochiengaoron@gmail.com>
2024-07-18 10:12:12 +0300

    Fix: fixed error msg and added newline

--- src/auth.c
+++ src/auth.c
@@ -44,7 +44,7 @@ void registerMenu(char *a, char *pass) {
   rot13(pass);
   int rc = insert_user(a, pass);
   if (rc == SQLITE_CONSTRAINT) {
-    printf("\n\t\tUser already exist!\n");
+    printf("\n\t\tUser already exist!\n\n\n");
     exit(0);
   }
 }
