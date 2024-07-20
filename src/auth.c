f0e60b78e73349c0b24536d1c5f48fdc035c9662
Aaron Ochieng <ochiengaoron@gmail.com>
2024-07-20 08:51:26 +0300

    Refractor: Aligned the login prompt Aligned the login prompt to start at the same margin.

--- src/auth.c
+++ src/auth.c
@@ -7,7 +7,7 @@ void loginMenu(char a[50], char pass[50]) {
   struct termios oflags, nflags;
 
   system("clear");
-  printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
+  printf("\n\t\t Bank Management System\n\n\n\n\t\t User Login: ");
   scanf("%s", a);
 
   // disabling echo
@@ -20,7 +20,7 @@ void loginMenu(char a[50], char pass[50]) {
     perror("tcsetattr");
     return exit(1);
   }
-  printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
+  printf("\n\t\t Enter the password to login: ");
   scanf("%s", pass);
 
   // restore terminal
@@ -32,7 +32,6 @@ void loginMenu(char a[50], char pass[50]) {
 
 const char *getPassword(struct User u) {
   char *password = get_user_pass(u.name);
-  rot13(password);
   return password;
 }
 void registerMenu(char *a, char *pass) {
@@ -41,10 +40,9 @@ void registerMenu(char *a, char *pass) {
   printf("\n\n\t\tEnter password:\t");
   scanf("%s", pass);
 
-  rot13(pass);
   int rc = insert_user(a, pass);
   if (rc == SQLITE_CONSTRAINT) {
-    printf("\n\t\tUser already exist!\n\n\n");
+    printf("\n\t\tUser already exist");
     exit(0);
   }
 }
